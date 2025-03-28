a7ccd5cb894f8dfd678cc40d47e529d5b8193c76
SteMarco07 <marcoste2007@gmail.com>
2025-03-28 17:12:03 +0100

    Aggiunta delle principali funzioni nella grafica

--- enigma/src/main/java/stellino/marco/enigma/EnigmaApplication.java
+++ enigma/src/main/java/stellino/marco/enigma/EnigmaApplication.java
@@ -11,8 +11,8 @@ public class EnigmaApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EnigmaApplication.class.getResource("enigma-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
+        stage.setTitle("Enigma");
         stage.setScene(scene);
         stage.show();
     }
--- enigma/src/main/java/stellino/marco/enigma/EnigmaController.java
+++ enigma/src/main/java/stellino/marco/enigma/EnigmaController.java
@@ -1,28 +1,102 @@
 package stellino.marco.enigma;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.transform.Rotate;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
 
 public class EnigmaController {
-    @FXML
-    private Label welcomeText;
-    @FXML
-    private Rotore rotore;
 
+
+    @FXML
+    private GridPane gridLamps;
+    @FXML
+    private GridPane gridButtons;
+
+    private Button[] buttons;
+    private Circle[] lamps;
 
     @FXML
     public void initialize() {
-        rotore = new Rotore("EKMFLGDQVZNTOWYHXUSPAIBRCJ");
+        gestisciGridButtons(60);
+        gestisciGridLamps(25);
     }
 
+    private void gestisciGridButtons(int dim) {
+        buttons = new Button[26];
+        char lettera = 'A';
+        gridButtons.setVgap(10);
+        gridButtons.setHgap(10);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                int indice = i * 9 + j;
+                buttons[indice] = new Button("" + lettera);
+                buttons[indice].setPrefWidth(dim);
+                buttons[indice].setPrefHeight(dim/2);
+                final char letteraF = lettera;
+                buttons[indice].setOnAction(e -> {
+                    System.out.println("Hai premuto la lettera " + letteraF);
+                    for (Circle lamp : lamps) {
+                        lamp.setFill(Paint.valueOf("#ffffff"));
+                    }
+                    lamps[indice].setFill(Color.YELLOW);
+                });
+                gridButtons.add(buttons[indice], j, i);
+                lettera++;
+                if (lettera == '[') return;
+            }
+        }
+    }
 
+    private void gestisciGridLamps(int dim) {
+        lamps = new Circle[26];
+        char lettera = 'A';
+        gridLamps.setVgap(10);
+        gridLamps.setHgap(20);
+
+        // Imposta l'allineamento al centro per tutte le celle della griglia
+        gridLamps.setAlignment(Pos.CENTER);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                // Crea il cerchio
+                Circle circle = new Circle(dim);
+                circle.setFill(Paint.valueOf("#ffffff"));
+                circle.setStroke(Paint.valueOf("#000000"));
+
+                // Crea la label con la lettera
+                Label label = new Label(String.valueOf(lettera));
+
+
+                // Crea un container StackPane per centrare gli elementi
+                StackPane stackPane = new StackPane();
+                stackPane.setAlignment(Pos.CENTER);  // Centra sia cerchio che label
+                stackPane.getChildren().addAll(circle, label);
+
+                // Aggiungi alla griglia
+                gridLamps.add(stackPane, j, i);
+
+                // Memorizza il cerchio nell'array
+                lamps[i * 9 + j] = circle;
+
+                lettera++;
+                if (lettera == '[') return;
+            }
+        }
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        this.rotore.ruota();
-        String stringa = (char)(this.rotore.get_uscita_dritto('a')) + " numero: " + this.rotore.get_rotazione();
-        welcomeText.setText(stringa);
-
+    public void onKeyPressed(KeyEvent event){
+        if (event.getCode().isLetterKey()){
+            int pos = event.getCode().getChar().charAt(0) - 'A';
+            buttons[pos].fire();
+            buttons[pos].requestFocus();
+        }
     }
 }
